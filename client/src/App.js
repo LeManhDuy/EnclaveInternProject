45b7768d047dbfc8ccf3f98d1916b7ec2622f40b
DinhNVT <huudinhnguyen001k@gmail.com>
2022-06-28 14:10:51 +0700

    [BT-39] Create base FONTEND project

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './logo.svg'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>We are blue team</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
