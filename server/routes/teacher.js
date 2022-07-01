93022e7f8cabcee9165afc8b98ceb13ff8c9db38
Lê Mạnh Duy <lemanhduy311111@gmail.com>
2022-07-01 16:22:05 +0700

    [BT-56] up model

--- server/routes/teacher.js
+++ server/routes/teacher.js
@@ -2,9 +2,25 @@ const express = require('express')
 const router = express.Router()
 const { authTeacher } = require('../middleware/verifyRoles')
 const verifyJWT = require('../../server/middleware/verifyJWT')
-
-router.post('/teacher', verifyJWT, authTeacher("Teacher"), (req, res) => {
-    res.send('Teacher page')
+const Student = require('../model/Student')
+const Class = require('../model/Class')
+// @route GET api/dashboard/teacher
+// @desc create student information
+// @access Private
+router.post('/teacher', verifyJWT, authTeacher("Teacher"), async (req, res) => {
+    const { student_fullname, student_age, student_gender, student_image, student_behavior } = req.body
+    //Simple validation
+    if (!student_fullname || !student_age || !student_gender || !student_image || !student_behavior)
+        return res.status(400).json({ success: false, message: 'Please fill in complete information' })
+    try {
+        const newStudent = new Student({
+            student_fullname, student_age, student_gender, student_image, student_behavior, teacher_id : req.userId, 
+        })
+        await newStudent.save()
+        res.json({ success: true, message: 'Create student successfully', student: newStudent })
+    } catch (error) {
+        return res.status(500).json({ success: false, message: '' + error })
+    }
 })
 
 module.exports = router
