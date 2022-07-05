19c69408b821fa24561eb1d3d2d2d7cf2a72ce09
Cao Minh Tri <caominhtri112201@gmail.com>
2022-07-05 14:12:06 +0700

    [BT-61] CRUD Grade

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const adminParentsRouter = require('./routes/parents')
 const adminRouterControl = require('./routes/admin')
 const authRouter = require('./routes/auth')
 const adminTeachersRouter = require('./routes/teacher')
+const gradeRouter = require('./routes/grade')
 const cors = require('cors')
 const app = express()
 app.use(express.json())
@@ -45,7 +46,7 @@ app.use('/api/admin/teachers', adminTeachersRouter)
 // app.use('/api/auth', authRouter)
 app.use('/api/admin/parents', adminParentsRouter)
 // app.use('/api/dashboard/', teacherRouter)
-
+app.use('/api/grades', gradeRouter)
 
 // login -> dahsboard -> teacher page (neu admin thi khong the den teacherpage) -> student page. 
 // app.get('/dashboard', (req, res) => { 
