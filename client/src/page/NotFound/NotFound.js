fefd2b860f7087b50ab82b20df60414a50b6628f
NhatTan <hoangnhattan31012001@gmail.com>
2022-07-08 10:24:17 +0700

    [BT-74] Implement UI Not Found page

--- client/src/page/NotFound/NotFound.js
+++ client/src/page/NotFound/NotFound.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import './NotFound.css'
-import ROUTES from "../../constants/routes";
-import { useHistory } from "react-router-dom";
+import ROUTES from "../../constants/routes"
+import { useHistory } from "react-router-dom"
+import Picture404 from '../../assets/image/404.png'
+import PictureNotFound from '../../assets/image/not_found_page_1.png'
 
 function NotFound() {
     const history = useHistory();
@@ -10,8 +12,17 @@ function NotFound() {
     }
     return (
         <div>
-            404
-            <button onClick = {HandleBackToHome}>Back to Home</button>
+            <div class="common">
+                <div class="img_not_found">
+                    <img src={PictureNotFound} />
+                </div>
+                <div class="body_not_found">
+                    <img src={Picture404}/>
+                    <h2>Hmm...</h2>
+                    <p>Look like the page you were looking for is no longer here.</p>
+                    <button class="btnBack" onClick = {HandleBackToHome}>Back to Home</button>
+                </div>
+            </div>
         </div>
     )
 }
