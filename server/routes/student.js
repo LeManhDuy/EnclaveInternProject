7872b08eb3bcb217e4b0bc2783191797e811ea76
Cao Minh Tri <caominhtri112201@gmail.com>
2022-07-12 11:31:03 +0700

    [BT-70] Auto Create Summary

--- server/routes/student.js
+++ server/routes/student.js
@@ -1,83 +1,119 @@
-require('dotenv').config()
-const express = require('express')
-const router = express.Router()
-const verifyJWTandTeacher = require('../middleware/verifyJWTandTeacher')
-const Student = require('../model/Student')
-const Teacher = require('../model/Teacher')
-const Class = require('../model/Class')
-const { ObjectId } = require('mongodb');
-const Subject = require('../model/Subject')
-const Grades = require('../model/Grade')
-const Score = require('../model/Score')
+require("dotenv").config();
+const express = require("express");
+const router = express.Router();
+const verifyJWTandTeacher = require("../middleware/verifyJWTandTeacher");
+const Student = require("../model/Student");
+const Teacher = require("../model/Teacher");
+const Class = require("../model/Class");
+const { ObjectId } = require("mongodb");
+const Subject = require("../model/Subject");
+const Grades = require("../model/Grade");
+const Score = require("../model/Score");
 // @route GET dashboard/teacher/create-student
 // @desc create student information
 // @access Private
-router.post('/create-student/:teacherID', verifyJWTandTeacher, async (req, res) => {
-    const { teacherID } = req.params
-    const { student_fullname, student_age, student_gender, student_image, student_behavior } = req.body
-    //Simple validation
-    if (!student_fullname || !student_age || student_gender == null || !student_image)
-        return res.status(400).json({ success: false, message: 'Please fill in complete information' })
-    try {
-        const teacher = await Teacher.findById(teacherID)
-        //save collection
-        const newStudent = new Student({
+router.post(
+    "/create-student/:teacherID",
+    verifyJWTandTeacher,
+    async (req, res) => {
+        const { teacherID } = req.params;
+        const {
             student_fullname,
             student_age,
             student_gender,
             student_image,
-            teacher_id: teacher._id,
-        })
-        await newStudent.save()
-        teacher.students.push(newStudent._id)
-        await teacher.save()
-        res.json({
-            success: true, message: 'Create student successfully',
-            studentFullName: newStudent.student_fullname,
-            studentAge: newStudent.student_age,
-            studentGender: newStudent.student_gender,
-            studentImage: newStudent.student_image,
-            teacherName: teacher.teacher_name
-        })
-    } catch (error) {
-        return res.status(500).json({ success: false, message: '' + error })
+            student_behavior,
+        } = req.body;
+        //Simple validation
+        if (
+            !student_fullname ||
+            !student_age ||
+            student_gender == null ||
+            !student_image
+        )
+            return res
+                .status(400)
+                .json({
+                    success: false,
+                    message: "Please fill in complete information",
+                });
+        try {
+            const teacher = await Teacher.findById(teacherID);
+            //save collection
+            const newStudent = new Student({
+                student_fullname,
+                student_age,
+                student_gender,
+                student_image,
+                teacher_id: teacher._id,
+            });
+            await newStudent.save();
+            teacher.students.push(newStudent._id);
+            await teacher.save();
+            res.json({
+                success: true,
+                message: "Create student successfully",
+                studentFullName: newStudent.student_fullname,
+                studentAge: newStudent.student_age,
+                studentGender: newStudent.student_gender,
+                studentImage: newStudent.student_image,
+                teacherName: teacher.teacher_name,
+            });
+        } catch (error) {
+            return res
+                .status(500)
+                .json({ success: false, message: "" + error });
+        }
     }
-})
+);
 
 // @route GET dashboard/teacher/get-student-by-teacher-id
 // @desc get student information
 // @access Private
-router.get('/get-student-by-teacher-id/:teacherID', verifyJWTandTeacher, async (req, res) => {
-    const { teacherID } = req.params
-    try {
-        const teacher = await Teacher.findById(teacherID)
-            .populate("students", ["_id", "student_fullname", "student_gender", "student_image"])
+router.get(
+    "/get-student-by-teacher-id/:teacherID",
+    verifyJWTandTeacher,
+    async (req, res) => {
+        const { teacherID } = req.params;
+        try {
+            const teacher = await Teacher.findById(teacherID).populate(
+                "students",
+                ["_id", "student_fullname", "student_gender", "student_image"]
+            );
 
-        const arrStudentId = []
-        teacher.students.map(item => {
-            arrStudentId.push(item._id)
-        })
+            const arrStudentId = [];
+            teacher.students.map((item) => {
+                arrStudentId.push(item._id);
+            });
 
-        const getStudentById = await Student.find({ '_id': arrStudentId })
-            .populate("class_id", ["class_name"])
-            .populate("teacher_id", ["teacher_name"])
-            .populate("subjects", ["subject_name"])
-            .populate("summary", ["summary_score", "summary_behavior"])
-            .select(["student_fullname", "student_age", "student_gender", "student_image"])
+            const getStudentById = await Student.find({ _id: arrStudentId })
+                .populate("class_id", ["class_name"])
+                .populate("teacher_id", ["teacher_name"])
+                .populate("subjects", ["subject_name"])
+                .populate("summary", ["summary_score", "summary_behavior"])
+                .select([
+                    "student_fullname",
+                    "student_age",
+                    "student_gender",
+                    "student_image",
+                ]);
 
-        return res.status(200).json({
-            teacherName: teacher.teacher_name,
-            studentInformation: getStudentById
-        })
-    } catch (error) {
-        return res.status(500).json({ success: false, message: '' + error })
+            return res.status(200).json({
+                teacherName: teacher.teacher_name,
+                studentInformation: getStudentById,
+            });
+        } catch (error) {
+            return res
+                .status(500)
+                .json({ success: false, message: "" + error });
+        }
     }
-})
+);
 
 // @route GET dashboard/teacher/get-all-student
 // @desc get student information
 // @access Private
-router.get('/get-all-student', verifyJWTandTeacher, async (req, res) => {
+router.get("/get-all-student", verifyJWTandTeacher, async (req, res) => {
     try {
         // Return token
         const allStudent = await Student.find({})
@@ -85,96 +121,133 @@ router.get('/get-all-student', verifyJWTandTeacher, async (req, res) => {
             .populate("teacher_id", ["teacher_name"])
             .populate("subjects", ["subject_name"])
             .populate("summary", ["summary_score", "summary_behavior"])
-            .select(["student_fullname", "student_age", "student_gender", "student_image"])
-        return res.status(200).json(
-            {
-                allStudent
-            })
+            .select([
+                "student_fullname",
+                "student_age",
+                "student_gender",
+                "student_image",
+            ]);
+        return res.status(200).json({
+            allStudent,
+        });
     } catch (error) {
-        return res.status(500).json({ success: false, message: '' + error })
+        return res.status(500).json({ success: false, message: "" + error });
     }
-})
+});
 
 // @route GET dashboard/teacher/get-student-by-id
 // @desc get student information by id
 // @access Private
-router.get('/get-student-by-student-id/:studentID', verifyJWTandTeacher, async (req, res) => {
-    const { studentID } = req.params
-    try {
-        // Return token
-        //sutdent
-        const getStudentById = await Student.findById(studentID)
-            .populate("class_id", ["class_name"])
-            .populate("teacher_id", ["teacher_name"])
-            .populate("subjects", ["score_id"])
-            .populate("summary", ["summary_score", "summary_behavior"])
-            .select(["student_fullname", "student_age", "student_gender", "student_image"])
-        //score
-        const arrScoreId = []
-        getStudentById.subjects.map(item => {
-            arrScoreId.push(item.score_id)
-        })
-        const getScoreById = await Score.find({ '_id': arrScoreId }).populate("subject_id", ["subject_name", "subject_ratio"]).select("score_average")
-        if (!getStudentById)
-            return res.status(401).json({ success: false, message: 'Student is not found!' })
-        return res.status(200).json({
-            studentInformation: getStudentById,
-            score_average: getScoreById
-        })
-    } catch (error) {
-        return res.status(500).json({ success: false, message: '' + error })
+router.get(
+    "/get-student-by-student-id/:studentID",
+    verifyJWTandTeacher,
+    async (req, res) => {
+        const { studentID } = req.params;
+        try {
+            // Return token
+            //student
+            const getStudentById = await Student.findById(studentID)
+                .populate("class_id", ["class_name"])
+                .populate("teacher_id", ["teacher_name"])
+                .populate("subjects", ["score_id"])
+                .populate("summary", ["summary_score", "summary_behavior"])
+                .select([
+                    "student_fullname",
+                    "student_age",
+                    "student_gender",
+                    "student_image",
+                ]);
+            //score
+            const arrScoreId = [];
+            getStudentById.subjects.map((item) => {
+                arrScoreId.push(item.score_id);
+            });
+            const getScoreById = await Score.find({ _id: arrScoreId })
+                .populate("subject_id", ["subject_name", "subject_ratio"])
+                .select("score_average");
+            if (!getStudentById)
+                return res
+                    .status(401)
+                    .json({ success: false, message: "Student is not found!" });
+            return res.status(200).json({
+                studentInformation: getStudentById,
+                score_average: getScoreById,
+            });
+        } catch (error) {
+            return res
+                .status(500)
+                .json({ success: false, message: "" + error });
+        }
     }
-})
+);
 
 // @route PUT dashboard/teacher/update-student
 // @desc Update stduent
 // @access Private Only Admin
-router.put('/update-student/:id', verifyJWTandTeacher, async (req, res) => {
-    const {
-        student_fullname,
-        student_age,
-        student_gender,
-        student_image } = req.body
+router.put("/update-student/:id", verifyJWTandTeacher, async (req, res) => {
+    const { student_fullname, student_age, student_gender, student_image } =
+        req.body;
     // Validation
-    if (!student_fullname || !student_age || student_gender == null || !student_image)
-        return res.status(400).json({ success: false, message: 'Please fill in complete information' })
+    if (
+        !student_fullname ||
+        !student_age ||
+        student_gender == null ||
+        !student_image
+    )
+        return res
+            .status(400)
+            .json({
+                success: false,
+                message: "Please fill in complete information",
+            });
     try {
         let updateStudent = {
             student_fullname,
             student_age,
             student_gender,
-            student_image
-        }
-        const postUpdateCondition = { _id: req.params.id, user: req.userId }
-        updatedStudent = await Student.findOneAndUpdate(postUpdateCondition, updateStudent, { new: true })
+            student_image,
+        };
+        const postUpdateCondition = { _id: req.params.id, user: req.userId };
+        updatedStudent = await Student.findOneAndUpdate(
+            postUpdateCondition,
+            updateStudent,
+            { new: true }
+        );
 
         if (!updateStudent)
-            return res.status(401).json({ success: false, message: 'Student is not found' })
+            return res
+                .status(401)
+                .json({ success: false, message: "Student is not found" });
         res.json({
-            success: true, message: 'Update succesfully!',
+            success: true,
+            message: "Update succesfully!",
             studentFullName: updatedStudent.student_fullname,
             studentAge: updatedStudent.student_age,
             studentGender: updatedStudent.student_gender,
-            studentImage: updatedStudent.student_image
-        })
+            studentImage: updatedStudent.student_image,
+        });
     } catch (error) {
-        return res.status(500).json({ success: false, message: '' + error })
+        return res.status(500).json({ success: false, message: "" + error });
     }
-})
+});
 
 // @route DELETE dashboard/teacher/delete-student
 // @desc delete student
 // @access Private
-router.delete('/delete-student/:id', verifyJWTandTeacher, async (req, res) => {
+router.delete("/delete-student/:id", verifyJWTandTeacher, async (req, res) => {
     try {
-        const postDeleteCondition = { _id: req.params.id, user: req.userId }
-        const deleteStudent = await Student.findOneAndDelete(postDeleteCondition)
+        const postDeleteCondition = { _id: req.params.id, user: req.userId };
+        const deleteStudent = await Student.findOneAndDelete(
+            postDeleteCondition
+        );
         if (!deleteStudent) {
-            return res.status(401).json({ success: false, message: "Student not found!" })
+            return res
+                .status(401)
+                .json({ success: false, message: "Student not found!" });
         }
-        res.json({ success: true, message: "Delete succesfully!" })
+        res.json({ success: true, message: "Delete succesfully!" });
     } catch (error) {
-        return res.status(500).json({ success: false, message: '' + error })
+        return res.status(500).json({ success: false, message: "" + error });
     }
-})
-module.exports = router
+});
+module.exports = router;
