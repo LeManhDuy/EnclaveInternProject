f67a2ca54720a57606fffa5f7bea7332bfdae15e
Cao Minh Tri <caominhtri112201@gmail.com>
2022-07-25 15:16:58 +0700

    [BT-107] Implement Add Subject

--- client/src/config/service/AccountService.js
+++ client/src/config/service/AccountService.js
@@ -1,15 +1,19 @@
 import HandleApi from "../api/HandleAPI";
 
 const getAccountsParents = async () => {
-  return await HandleApi.APIGetWithToken("admin/parents");
+    return await HandleApi.APIGetWithToken("admin/parents");
 };
 
 const getAccountsAdmin = async () => {
-  return await HandleApi.APIGetWithToken("admin");
+    return await HandleApi.APIGetWithToken("admin");
 };
 
 const getAccountsTeacher = async () => {
-  return await HandleApi.APIGetWithToken("admin/teachers");
+    return await HandleApi.APIGetWithToken("admin/teachers");
+};
+
+const getFreeTeacher = async () => {
+    return await HandleApi.APIGetWithToken("admin/teachers/get-teacher/");
 };
 
 // const getAccountById = async (id) => {
@@ -17,46 +21,46 @@ const getAccountsTeacher = async () => {
 // }
 
 const deleteAccountParentsById = async (id) => {
-  return await HandleApi.APIDelete(`admin/parents/${id}`)
-}
+    return await HandleApi.APIDelete(`admin/parents/${id}`);
+};
 
 const deleteAccountAdminById = async (id) => {
-  return await HandleApi.APIDelete(`admin/${id}`)
-}
+    return await HandleApi.APIDelete(`admin/${id}`);
+};
 
 const deleteAccountTeacherById = async (id) => {
-  return await HandleApi.APIDelete(`admin/teachers/${id}`)
-}
+    return await HandleApi.APIDelete(`admin/teachers/${id}`);
+};
 
 const addAccountAdmin = async (params) => {
-  return await HandleApi.APIPostWithToken(`admin`, params)
-}
+    return await HandleApi.APIPostWithToken(`admin`, params);
+};
 
 const addAccountParents = async (params) => {
-  return await HandleApi.APIPostWithTokenIMG(`admin/parents`, params)
-}
+    return await HandleApi.APIPostWithTokenIMG(`admin/parents`, params);
+};
 
 const addAccountTeacher = async (params) => {
-  return await HandleApi.APIPostWithTokenIMG(`admin/teachers`, params)
-}
+    return await HandleApi.APIPostWithTokenIMG(`admin/teachers`, params);
+};
 
 // const updateProfile = async (params) => {
 //   return await HandleApi.APIPutWithToken("accounts", params)
 // }
 
 const AccountService = {
-  getAccountsParents,
-  getAccountsAdmin,
-  getAccountsTeacher,
-  //  getAccountById,
-  //   updateProfile,
-  deleteAccountParentsById,
-  deleteAccountAdminById,
-  deleteAccountTeacherById,
-  addAccountAdmin,
-  addAccountParents,
-  addAccountTeacher
-
+    getAccountsParents,
+    getAccountsAdmin,
+    getAccountsTeacher,
+    //  getAccountById,
+    //   updateProfile,
+    deleteAccountParentsById,
+    deleteAccountAdminById,
+    deleteAccountTeacherById,
+    addAccountAdmin,
+    addAccountParents,
+    addAccountTeacher,
+    getFreeTeacher,
 };
 
 export default AccountService;
