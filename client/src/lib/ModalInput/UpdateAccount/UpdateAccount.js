563cf406082f1f0314309008b8e39464b757bdd7
DinhNVT <huudinhnguyen001k@gmail.com>
2022-07-26 08:38:58 +0700

    [BT-109] Implement Edit Teacher Account

--- client/src/lib/ModalInput/UpdateAccount/UpdateAccount.js
+++ client/src/lib/ModalInput/UpdateAccount/UpdateAccount.js
@@ -4,6 +4,7 @@ import Logo from "../../../assets/image/Logo.png";
 import AccountService from "../../../config/service/AccountService";
 
 const UpdateAccount = (props) => {
+  const { REACT_APP_API_ENDPOINT } = process.env;
   const [allValuesAdmin, setAllValuesAdmin] = useState({
     name: "",
     email: "",
@@ -25,7 +26,6 @@ const UpdateAccount = (props) => {
     password: "",
     confirmPassword: "",
     img: null,
-    urlIMG: "",
   });
   const [teacherError, setTeacherError] = useState({
     name: false,
@@ -37,6 +37,8 @@ const UpdateAccount = (props) => {
     confirmPassword: false,
     img: false,
   });
+  const [avatar, setAvatar] = useState(Logo);
+
   // const [allValuesParents, setAllValuesParents] = useState({
   //   name: "",
   //   dateOfBirth: `${date.split("/")[2]}-${
@@ -65,12 +67,21 @@ const UpdateAccount = (props) => {
     confirmPassword: false,
     img: false,
   });
-  
+
   const validateEmail = (email) => {
     const re =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   };
+
+  const toDataURL = url => fetch(url)
+      .then(response => response.blob())
+      .then(blob => new Promise((resolve, reject) => {
+      const reader = new FileReader()
+      reader.onloadend = () => resolve(reader.result)
+      reader.onerror = reject
+      reader.readAsDataURL(blob)
+     }))
 
   useEffect(() => {
     if (props.dropValue === "admin") {
@@ -83,21 +94,21 @@ const UpdateAccount = (props) => {
         })
       );
     } else if (props.dropValue === "teacher") {
-      AccountService.getAccountsTeacherById(props.AccountId).then((res) =>
+      AccountService.getAccountsTeacherById(props.AccountId).then((res) => {
+        (!!res.teacher.teacher_img)
+          ? (setAvatar(`${REACT_APP_API_ENDPOINT}${res.teacher.teacher_img}`))
+          :setAvatar(Logo);
         setAllValuesTeacher({
           name: res.teacher.teacher_name,
           age: res.teacher.teacher_age,
-          gender: res.teacher.teacher_gender,
+          gender: `${res.teacher.teacher_gender}`,
           phone: res.teacher.teacher_phone,
           email: res.teacher.teacher_email,
           password: "",
           confirmPassword: "",
-          img: `${process.env}${res.teacher.teacher_img}`,
-          urlIMG: res.teacher.teacher_img
-        })
-      );
-    } else {
-
+          img: null,
+        });
+      });
     }
   }, []);
 
@@ -136,10 +147,89 @@ const UpdateAccount = (props) => {
     }
   };
 
+  const handleUpdateTeacherAccount = () => {
+    let name = false;
+    let age = false;
+    let gender = false;
+    let phone = false;
+    let email = false;
+    let password = false;
+    let confirmPassword = false;
+    let img = false;
+
+    let check = false;
+    if (allValuesTeacher.name.length > 30 || allValuesTeacher.name.length < 2) {
+      name = true;
+      check = true;
+    } else name = false;
+
+    if (validateEmail(allValuesTeacher.email) === false) {
+      email = true;
+      check = true;
+    } else email = false;
+
+    if (allValuesTeacher.password.length < 6) {
+      password = true;
+      check = true;
+    } else if (allValuesTeacher.confirmPassword != allValuesTeacher.password) {
+      confirmPassword = true;
+      check = true;
+    } else {
+      (password = false), (confirmPassword = false);
+    }
+
+    if (
+      isNaN(parseInt(allValuesTeacher.age)) ||
+      allValuesTeacher.age.length > 3
+    ) {
+      age = true;
+      check = true;
+    } else age = false;
+
+    console.log(allValuesTeacher.gender)
+    if (allValuesTeacher.gender==null) {
+      gender = true;
+      check = true;
+    } else gender = false;
+
+    if (
+      isNaN(parseInt(allValuesTeacher.phone)) ||
+      allValuesTeacher.phone.length != 10
+    ) {
+      phone = true;
+      check = true;
+    } else phone = false;
+
+    if (!!allValuesTeacher.img) {
+      let imgList = allValuesTeacher.img.name.split(".");
+      if (
+        imgList[imgList.length - 1] != "png" &&
+        imgList[imgList.length - 1] != "jpg"
+      ) {
+        img = true;
+        check = true;
+      } else img = false;
+    }
+
+    setTeacherError({
+      name: name,
+      age: age,
+      gender: gender,
+      phone: phone,
+      email: email,
+      password: password,
+      confirmPassword: confirmPassword,
+      img: img,
+    });
+    if (!check) {
+      props.handleConfirmUpdateAccount(allValuesTeacher);
+    }
+  };
+
   const clickUpdate = (e) => {
     e.preventDefault();
     if (props.dropValue === "admin") handleUpdateAdminAccount();
-    // else if (props.dropValue === "teacher") handleAddTeacherAccount();
+    else if (props.dropValue === "teacher") handleUpdateTeacherAccount();
     // else handleAddParentsAccount();
   };
 
@@ -227,7 +317,208 @@ const UpdateAccount = (props) => {
     </div>
   );
 
-  const FormAccountTeacher = <div>teacher</div>;
+  const changeHandlerTeacher = (e) => {
+    setAllValuesTeacher({
+      ...allValuesTeacher,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const changeHandlerTeacherIMG = (e) => {
+    setAllValuesTeacher({
+      name: allValuesTeacher.name,
+      age: allValuesTeacher.age,
+      gender: allValuesTeacher.gender,
+      phone: allValuesTeacher.phone,
+      email: allValuesTeacher.email,
+      password: allValuesTeacher.password,
+      confirmPassword: allValuesTeacher.confirmPassword,
+      img: e.target.files[0],
+    });
+    try {
+      setAvatar(URL.createObjectURL(e.target.files[0]));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const FormAccountTeacher = (
+    <div className="form-admin-content">
+      <h2>Add Account Teacher</h2>
+      <label
+        className={
+          "error" + (props.errorServer ? " error-show" : " error-hidden")
+        }
+      >
+        Account already exists
+      </label>
+      <div className="form-teacher-content">
+        <div className="teacher-content-left">
+          <div className="avatar-teacher">
+            <img src={avatar} />
+            <label className="choose-file" htmlFor="upload-photo">
+              Choose image
+            </label>
+            <input
+              id="upload-photo"
+              type="file"
+              name="img"
+              onChange={changeHandlerTeacherIMG}
+            />
+            <label
+              className={
+                "error" + (teacherError.img ? " error-show" : " error-hidden")
+              }
+            >
+              The selected file is not valid
+            </label>
+          </div>
+          <div className="type-input">
+            <h4>Name</h4>
+            <input
+              className="input-content"
+              type="text"
+              name="name"
+              placeholder="Enter name"
+              value={allValuesTeacher.name}
+              onChange={changeHandlerTeacher}
+            />
+            <label
+              className={
+                "error" + (teacherError.name ? " error-show" : " error-hidden")
+              }
+            >
+              Name must be less than 30 chars
+            </label>
+          </div>
+          <div className="type-input">
+            <h4>Age</h4>
+            <input
+              className="input-content"
+              type="text"
+              name="age"
+              placeholder="Enter age"
+              value={allValuesTeacher.age}
+              onChange={changeHandlerTeacher}
+            />
+            <label
+              className={
+                "error" + (teacherError.age ? " error-show" : " error-hidden")
+              }
+            >
+              Invalid age
+            </label>
+          </div>
+          <div className="type-input">
+            <h4>Gender</h4>
+            <div className="radio-btn">
+              <div className="radio">
+                <input
+                  type="radio"
+                  value="true"
+                  name="gender"
+                  onChange={changeHandlerTeacher}
+                  checked={allValuesTeacher.gender==="true"}
+                />
+                Male
+                <input
+                checked = {allValuesTeacher.gender==="false"}
+                  type="radio"
+                  value="false"
+                  name="gender"
+                  onChange={changeHandlerTeacher}
+                />
+                Female
+              </div>
+              <label
+                className={
+                  "error" +
+                  (teacherError.gender ? " error-show" : " error-hidden")
+                }
+              >
+                No gender selected
+              </label>
+            </div>
+          </div>
+        </div>
+        <div className="teacher-content-right">
+          <div className="type-input">
+            <h4>Phone Number</h4>
+            <input
+              className="input-content"
+              type="text"
+              name="phone"
+              placeholder="Enter phone"
+              value={allValuesTeacher.phone}
+              onChange={changeHandlerTeacher}
+            />
+            <label
+              className={
+                "error" + (teacherError.phone ? " error-show" : " error-hidden")
+              }
+            >
+              Phone must be 10 numeric characters
+            </label>
+          </div>
+          <div className="type-input">
+            <h4>Email</h4>
+            <input
+              className="input-content"
+              type="email"
+              name="email"
+              placeholder="Enter email"
+              value={allValuesTeacher.email}
+              onChange={changeHandlerTeacher}
+            />
+            <label
+              className={
+                "error" + (teacherError.email ? " error-show" : " error-hidden")
+              }
+            >
+              Invalid Email
+            </label>
+          </div>
+          <div className="type-input">
+            <h4>Password</h4>
+            <input
+              className="input-content"
+              type="password"
+              name="password"
+              placeholder="Enter password "
+              value={allValuesTeacher.password}
+              onChange={changeHandlerTeacher}
+            />
+            <label
+              className={
+                "error" +
+                (teacherError.password ? " error-show" : " error-hidden")
+              }
+            >
+              Password must be at least 6 chars long
+            </label>
+          </div>
+          <div className="type-input">
+            <h4>Confirm Password</h4>
+            <input
+              className="input-content"
+              type="password"
+              name="confirmPassword"
+              placeholder="Enter password "
+              value={allValuesTeacher.confirmPassword}
+              onChange={changeHandlerTeacher}
+            />
+            <label
+              className={
+                "error" +
+                (teacherError.confirmPassword ? " error-show" : " error-hidden")
+              }
+            >
+              Password incorrect
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 
   const FormAccountParents = <div>Parents</div>;
 
---
+++ client/src/page/Parents/StudentParents/StudentParents.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './StudentsParents.css'
+
+const StudentParents = () => {
+  return (
+    <div>StudentParents</div>
+  )
+}
+
+export default StudentParents
