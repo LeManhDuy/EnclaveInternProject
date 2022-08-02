912f1f7a899a787eb9e33301e543b5ccada27215
Lê Mạnh Duy <lemanhduy311111@gmail.com>
2022-08-02 16:06:17 +0700

    make it better

--- server/server.js
+++ server/server.js
@@ -79,7 +79,7 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'))
     })
-}
+} 
 //port
 const PORT = 8000
 app.listen(PORT, () => console.log(`Server started ${PORT}`))
