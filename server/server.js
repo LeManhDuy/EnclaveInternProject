51d28f613f46a971ac96b06c8be2c0f8713a3f6b
Lê Mạnh Duy <64477259+LeManhDuy@users.noreply.github.com>
2022-08-09 10:29:52 +0700

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,88 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const adminParentsRouter = require("./routes/parents")
+const adminRouterControl = require("./routes/admin")
+const authRouter = require("./routes/auth")
+const adminTeachersRouter = require("./routes/teacher")
+const subjectRouter = require("./routes/subject")
+const classRouter = require("./routes/class")
+const gradeRouter = require("./routes/grade")
+const protectorRouter = require("./routes/protector")
+const studentRouter = require("./routes/student")
+const scoreRouter = require("./routes/score")
+const summaryRouter = require("./routes/summaryScore")
+const scheduleRouter = require("./routes/schedule")
+const notificationRouter = require("./routes/notification")
+const cors = require("cors")
+const path = require("path")
+const app = express()
+app.use(express.json())
+//Static Image
+app.use("/uploads/parents", express.static("uploads/parents"))
+app.use("/uploads/teachers", express.static("uploads/teachers"))
+app.use("/uploads/protectors", express.static("uploads/protectors"))
+app.use("/uploads/students", express.static("uploads/students"))
+app.use("/uploads/schedules", express.static("uploads/schedules"))
+
+//database
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            process.env.DATABASE
+        )
+        console.log("MongoDB connected")
+    } catch (error) {
+        console.log(error.message)
+        process.exit(1)
+    }
+}
+connectDB()
+
+//cors
+app.use(cors())
+app.get("/", (req, res) => {
+    res.json("Server Startedd")
+})
+//url
+//admin
+app.use("/api/admin/teachers", adminTeachersRouter)
+app.use("/api/admin/parents", adminParentsRouter)
+app.use("/api/authentication", authRouter)
+app.use("/api/admin/parents", adminParentsRouter)
+app.use("/api/admin/", adminRouterControl)
+//teacher
+// app.use('/api/dashboard/', teacherRouter)
+//student
+app.use("/api/student", studentRouter)
+//class
+app.use("/api/teacher/class", classRouter)
+//score
+app.use("/api/teacher/score", scoreRouter)
+
+// app.use('/api/auth', authRouter)
+app.use("/api/admin/parents", adminParentsRouter)
+app.use("/api/subjects", subjectRouter)
+// app.use('/api/dashboard/', teacherRouter)
+//subject
+app.use("/api/teacher/subjects", subjectRouter)
+//greades
+app.use("/api/grades", gradeRouter)
+app.use("/api/protectors", protectorRouter)
+//summary
+app.use("/api/summary", summaryRouter)
+//schedule
+app.use("/api/schedule", scheduleRouter)
+//notification
+app.use("/api/notification", notificationRouter)
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('./client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, './client', 'build', 'index.html'))
+    })
+}
+//port
+app.listen(process.env.PORT || 8000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
