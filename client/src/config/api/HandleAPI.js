c7ead84e76ddbfae6053f91cbbbe833e282d04b5
Lê Mạnh Duy <lemanhduy311111@gmail.com>
2022-08-10 10:21:08 +0700

    fix api

--- client/src/config/api/HandleAPI.js
+++ client/src/config/api/HandleAPI.js
@@ -1,7 +1,7 @@
 import AuthenticationService from "../service/AuthenticationService"
 
 //const { REACT_APP_API_ENDPOINT } = process.env
-const REACT_APP_API_ENDPOINT = "http://localhost:8000/"
+const REACT_APP_API_ENDPOINT = "https://blue-school-project.herokuapp.com/"
 
 const header = {
   Accept: "application/json",
