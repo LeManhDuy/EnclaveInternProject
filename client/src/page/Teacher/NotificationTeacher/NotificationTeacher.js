9ee0228aef99612aa2488980a5fab2bed1dc0d37
Lê Mạnh Duy <64477259+LeManhDuy@users.noreply.github.com>
2022-08-10 10:27:39 +0700

    Update NotificationTeacher.js

--- client/src/page/Teacher/NotificationTeacher/NotificationTeacher.js
+++ client/src/page/Teacher/NotificationTeacher/NotificationTeacher.js
@@ -120,7 +120,7 @@ const NotificationTeacher = () => {
                 <ConfirmAlert
                     handleCloseModalCustom={handleCloseModalCustom}
                     handleDelete={handleDelete}
-                    title={`Do you want to delete the ${name}?`}
+                    title={`Do you want to delete this notification ?`}
                 />
             }
             handleCloseModalCustom={handleCloseModalCustom}
